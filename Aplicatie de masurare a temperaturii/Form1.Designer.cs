13114fdf58e1149f5610949dad0d22550cd0289f
unknown <batiegeorge@yahoo.com>
2020-12-11 14:44:14 +0200

    First commit

---
+++ Aplicatie de masurare a temperaturii/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Aplicatie_de_masurare_a_temperaturii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(23, 32);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Legend = "Legend1";
+            series2.Name = "Temperatura";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(750, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(109, 378);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(285, 378);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM7";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Aplicatie de masurare a temperaturii/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicatie_de_masurare_a_temperaturii
+{
+    public partial class Form1 : Form
+    {
+        int cnt;
+        public Form1()
+        {
+            InitializeComponent();
+            cnt = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start(); // porneste timer-ul 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop(); //opreste timer-ul
+        }
+
+        double getTemp(string cmd)
+        {
+            double res=0;
+            string resDev = "";
+            if (serialPort1.IsOpen) // daca exista conexiune deschisa de la o executie anterioara atunci o inchide
+                serialPort1.Close();
+            serialPort1.Open(); // dechide conexiunea
+            serialPort1.Write(cmd + "\r"); // scrie comanda catre  microcontroller pe PORT impreuna cu caracterul terminal
+            while (!resDev.Contains("<")) // cat timp in resDev nu exista "<"     <-sfarsit de string din raspunsul microcontrollerului  
+                resDev += serialPort1.ReadLine();
+            resDev = resDev.Substring(0, resDev.IndexOf(".") + 2);// ia primele doua cifre inainte de punct si urmatoarele doua dupa punct ex : 23.2344
+            serialPort1.Close(); // inchide conexiunea
+            try
+            {
+                res = Convert.ToDouble(resDev);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return res;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            double temp = getTemp("t");
+            chart1.Series["Temperatura"].Points.AddXY(cnt, temp);
+        }
+    }
+}
